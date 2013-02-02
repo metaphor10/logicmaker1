afe901e3a14fc9ed73904c6ae780d58a9f972c8c
Michael Black <michaelblack@temp.home>
2013-02-02 00:09:18 -0500

    Initial Commit

---
+++ Blocky/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Blocky
+//
+//  Created by Michael Black on 2/2/13.
+//  Copyright (c) 2013 American University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
